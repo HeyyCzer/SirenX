948e7fb8c67ab0eab0121220a51f12d89c68bd5c
HeyyCzer <43618377+HeyyCzer@users.noreply.github.com>
2025-05-30 01:39:54 -0300

    🧪 improve tests

--- test/e2e/2-complex/import.cy.ts
+++ test/e2e/2-complex/import.cy.ts
@@ -25,28 +25,53 @@ context("Importing", () => {
 		cy.get("@fileInput").should("not.be.visible");
 		cy.get("@fileInput").selectFile("./test/assets/carcols.meta", { force: true });
 
+		// should show the import prompt dialog since the file includes multiple sirens
 		cy.get(".swal2-container").should("be.visible");
 		cy.get(".swal2-container .swal2-confirm").click();
 
 		const Colors = useColorStore.getState().colors;
 
 		const redLights = [
-			[0, 1, 15, 16, 17, 18],
-			[14],
-			[0, 1, 15, 16, 17, 18],
-			[14],
-			[2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13],
+			[0, 2, 4, 5, 6],
+			[1, 3, 9],
+			[4, 5],
+			[0, 1, 2, 3, 6, 9],
+			[4, 5],
+			[1, 3, 9],
+			[0, 2, 4, 5, 6],
+			[0, 1, 2, 3, 4, 5, 6, 9]
 		];
+
+		const whiteLights = [
+			[7],
+			[8],
+			[7],
+			[8],
+			[7],
+			[8],
+			[],
+			[7, 8],
+			[],
+			[8],
+			[7],
+			[8],
+			[7],
+		]
 
 		cy.get(".light-row").each(($row, index) => {
 			cy.wrap($row).children("button").should("have.length", 20);
 
 			cy.wrap($row).children().each(($light, lightIndex) => {
 				if (index >= redLights.length - 1) return;
+				if (index >= whiteLights.length - 1) return;
 
 				const classes = $light[0].className;
 
-				const expectedColor = redLights[index].includes(lightIndex) ? "red" : "none";
+				const expectsRed = redLights[index].includes(lightIndex);
+				const expectsWhite = whiteLights[index].includes(lightIndex);
+
+				const expectedColor = expectsRed ? "red" : expectsWhite ? "white" : null;
+				if (!expectedColor) return;
 
 				const color = Colors[expectedColor].editor.default;
 				const colorActive = Colors[expectedColor].editor.current;
@@ -61,5 +86,14 @@ context("Importing", () => {
 				expect(isValid, "Checking for color").to.be.true;
 			});
 		});
+	});
+
+	it("should import file with custom colors correctly", () => {
+		cy.get("input[type=file]").as("fileInput");
+
+		cy.get("@fileInput").should("not.be.visible");
+		cy.get("@fileInput").selectFile("./test/assets/carcols-custom-colors.meta", { force: true });
+
+		cy.get("#toolbar-colors").children().should("have.length", 10);
 	});
 });
