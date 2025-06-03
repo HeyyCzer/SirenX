8398ebf78466244af03329b57e44629b0e330a9f
HeyyCzer <43618377+HeyyCzer@users.noreply.github.com>
2025-06-03 12:07:39 -0300

    🐛 fix settings breaking after importing an invalid file

--- src/controllers/file.controller.js
+++ src/controllers/file.controller.js
@@ -9,7 +9,7 @@ const uploadFile = async (fileContent) => {
 	try {
 		xmlJson = xml2json(fileContent, { compact: true, attributesKey: "$" });
 	} catch {
-		return Modal.fire({
+		return void Modal.fire({
 			icon: 'error',
 			title: 'Error',
 			text: 'The file provided is not a valid XML file. Please, check the file and try again'
@@ -19,7 +19,7 @@ const uploadFile = async (fileContent) => {
 	const json = JSON.parse(xmlJson);
 	let sirens = json?.CVehicleModelInfoVarGlobal?.Sirens?.Item;
 	if (!sirens) {
-		return Modal.fire({
+		return void Modal.fire({
 			icon: 'error',
 			title: 'Error',
 			text: 'The file provided is not a valid carcols.meta file or does not contain any siren data. Please try another file.'
@@ -32,9 +32,7 @@ const uploadFile = async (fileContent) => {
 	});
 
 	const hasMultipleSirens = Array.isArray(sirens);
-	if (!hasMultipleSirens) {
-		sirens = [sirens];
-	}
+	if (!hasMultipleSirens) sirens = [sirens];
 
 	let selectedSiren = sirens[0];
 	if (sirens.length > 1) {
@@ -56,13 +54,12 @@ const uploadFile = async (fileContent) => {
 	}
 
 	if (!selectedSiren) return;
-
 	try {
 		return buildLights(selectedSiren, json);
 	} catch (err) {
-		Sentry.captureException(err);
+		Sentry.captureException(err, { level: "warning" });
 
-		await Modal.fire({
+		void Modal.fire({
 			icon: 'error',
 			title: 'Error while importing',
 			text: err.customMessage || 'Failed to load your file. Are you sure this is a valid carcols.meta file?'
@@ -94,7 +91,7 @@ const downloadFile = (editor, settings, fileName) => {
 	} catch (err) {
 		Sentry.captureException(err);
 
-		return Modal.fire({
+		return void Modal.fire({
 			icon: 'error',
 			title: 'Error while exporting',
 			text: err.customMessage || 'Error while trying to export the file. Please, try again or reset the editor.'
